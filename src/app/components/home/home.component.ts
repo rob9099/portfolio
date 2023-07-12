e2bc6450b4eb65db032636c111bbfc158cbe7fa5
Robert Lockett <lockettr@hotmail.com>
2023-07-12 20:51:44 +0200

    Done with checkbox button

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.pug',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.sass']
 })
 export class HomeComponent {
 
